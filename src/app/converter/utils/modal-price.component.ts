d1d8421247a837724230ab63888dd43bc3889db4
higorlopees <higorlopees@live.com>
2020-02-28 07:57:59 -0300

    Added modal-price component

---
+++ src/app/converter/utils/modal-price.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modal-price',
+  templateUrl: './modal-price.component.html',
+  styleUrls: ['./modal-price.component.css']
+})
+export class ModalPriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
